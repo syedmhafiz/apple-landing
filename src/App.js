41f3f33ef7dc7ad2a92e883b70b255fc4fa14985
syedmhafiz <syedhafiz61@gmail.com>
2024-04-30 19:30:05 +0600

    Rolled up the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Footer></Footer>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { appleImg } from "../utils";
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src={appleImg} alt="" />
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
